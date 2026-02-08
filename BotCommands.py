59d78414ca978bf41b71d6c3ce20de5178e7ce55
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-02-08 11:49:21 -0800

    add command for user tool install
    whispers this always unless you are a mod, then you can choose

--- BotCommands.py
+++ BotCommands.py
@@ -1,4 +1,5 @@
 # ScamGuard application commands that are used on every server
+from typing import cast
 from CommandHelpers import TargetIdTransformer
 from discord import app_commands, Interaction, Member, User, Embed
 from ScamReportModal import SubmitScamReport
@@ -101,7 +102,20 @@ class GlobalScamCommands(app_commands.Group):
       await self.GetInstance().ServerSetupHelper.OpenServerSetupModel(interaction)
     else:
       await interaction.response.send_message(Messages["cmds_error"]["server_activated"], ephemeral=True, delete_after=15.0)
-      
+     
+  @app_commands.command(name="tool", description="Provides a link to install the ScamGuard User Tool")
+  @app_commands.describe(whisper='If the link should be not whispered (only changeable if you are a mod)')
+  @app_commands.checks.cooldown(1, 2.0)
+  async def InstallScamGuardUser_Global(self, interaction:Interaction, whisper:bool):
+    # Always be a whisper unless you are an admin/mod
+    ShouldEphermeral:bool = True
+    # Was this in a server
+    if (interaction.guild_id is not None and interaction.channel is not None):
+      # Check to see if a moderator typed this command
+      if (interaction.channel.permissions_for(cast(Member, interaction.user)).ban_members):
+        ShouldEphermeral = whisper
+    
+    await interaction.response.send_message("https://discord.com/oauth2/authorize?client_id=1443130827662823557", delete_after=10.0, ephemeral=ShouldEphermeral)
       
   @app_commands.command(name="config", description="Set ScamGuard Settings")
   @app_commands.checks.has_permissions(ban_members=True)
