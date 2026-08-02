484492dc18d5e780adf9fd86c828cccc5f7e4aab
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-02 11:26:06 -0700

    Make note
    About why this type is string interpreted instead of being an exact class

--- Types.py
+++ Types.py
@@ -6,6 +6,9 @@ if TYPE_CHECKING:
   from ScamGuard import ScamGuard
   from BotBase import DiscordBot
 
+# this is hilariously valid because it uses annotations
+# we could probably just have it be of DiscordBot since ScamGuard inherits
+# but typing is silly.
 type BotType = 'DiscordBot|ScamGuard'
 type OptionalChannel = TextChannel|None
 type DiscordPerson = User|Member
